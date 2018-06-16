cbaba93263c8246a8231a4606ecf01f2d85482e3
smallwordWISH <word714cs@gmail.com>
2018-06-17 02:47:26 +0800

    add cart item quantity notice on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_cart
+  helper_method :current_cart 
+  helper_method :calculate_cart_total_quantity
 
   private
 
@@ -18,5 +19,14 @@ class ApplicationController < ActionController::Base
 
     session[:cart_id] = @cart.id
     @cart
+  end
+
+  def calculate_cart_total_quantity(cart_items)
+    totoal_quantity = 0
+    cart_items.each do |cart_item|
+      totoal_quantity += cart_item.quantity
+    end
+
+    totoal_quantity
   end
 end
